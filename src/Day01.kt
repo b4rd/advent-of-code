97566ded4d545344a48516dff5812517a4910839
JaKK <jakk87@gmail.com>
2023-12-02 11:26:17 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    assert(getCalibrationValue("threeight") == 38)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val testInput = readInput("Day01")
+    val result = testInput.sumOf { line -> getCalibrationValue(line) }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(result)
 }
+
+fun getCalibrationValue(line: String): Int {
+    val matches = numberRegex.findAll(line)
+    // The match is empty because all matchable characters are inside the lookahead assertion.
+    // Get the matches from the capturing group.
+    // https://mtsknn.fi/blog/how-to-do-overlapping-matches-with-regular-expressions/
+    val firstDigit = matches.first().groupValues[1].toDigit()
+    val lastDigit = matches.last().groupValues[1].toDigit()
+    return firstDigit * 10 + lastDigit
+}
+
+fun String.toDigit(): Int {
+    if (length == 1) {
+        return toInt()
+    }
+    return requireNotNull(spelledDigitsToNumbers[this])
+}
+
+val spelledDigitsToNumbers = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+)
+
+val numberRegex = """(?=(\d|one|two|three|four|five|six|seven|eight|nine))""".toRegex()
