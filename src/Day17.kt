db2741c62000cafdc635bdbc57d80d4d7b28d164
Peter Jakkel <pjakkel@ptc.com>
2023-12-17 10:09:09 +0100

    Day17

---
+++ src/Day17.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    val inputLines = readInput("Day17_test")
+            .map { it.map { char -> char.digitToInt() } }
+    var paths = mutableListOf<List<Position17>>()
+
+
+}
+
+private fun heatLossAt(inputLines: List<Int>, pos: Position17) {
+
+}
+
+private data class Position17(val x: Int, val y: Int)
+
+
